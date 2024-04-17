81a57868db7c5cfb9d63b5dfd6da7d6771993d20
kirubhel <61353074+kirubhel@users.noreply.github.com>
2024-04-17 13:23:38 +0300

    last-commit3

--- IntegratedDigitalAPI/Infrustructure/Migrations/20240411172540_BegningBalanceDetail.cs
+++ IntegratedDigitalAPI/Infrustructure/Migrations/20240411172540_BegningBalanceDetail.cs
@@ -25,16 +25,16 @@ namespace IntegratedInfrustructure.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_BegningBalanceDetails_PeriodDetails_ChartOfAccountId",
-                table: "BegningBalanceDetails");
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_BegningBalanceDetails_PeriodDetails_ChartOfAccountId",
+            //    table: "BegningBalanceDetails");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_BegningBalanceDetails_ChartOfAccounts_ChartOfAccountId",
-                table: "BegningBalanceDetails",
-                column: "ChartOfAccountId",
-                principalTable: "ChartOfAccounts",
-                principalColumn: "Id");
+            //migrationBuilder.AddForeignKey(
+            //    name: "FK_BegningBalanceDetails_ChartOfAccounts_ChartOfAccountId",
+            //    table: "BegningBalanceDetails",
+            //    column: "ChartOfAccountId",
+            //    principalTable: "ChartOfAccounts",
+            //    principalColumn: "Id");
         }
     }
 }
