1bb0a256a279fa44879bbf0f7a5664e128fa6546
Hermi21 <heermites@gmail.com>
2024-10-12 09:58:26 +0300

    VehicleDetail

---
+++ IntegratedDigitalAPI/Infrustructure/Model/HRM/OnFieldEmployeeList.cs
@@ -0,0 +1,17 @@
+﻿using IntegratedInfrustructure.Model.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratedInfrustructure.Model.HRM
+{
+    public class OnFieldEmployeeList: WithIdModel
+    {
+        public Guid EmployeeId { get; set; }
+        public virtual EmployeeList EmployeeList { get; set; } = null!;
+        public DateTime FieldDate { get; set; }
+
+    }
+}
